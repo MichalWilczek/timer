b59ddb6a923bf0db6b7e273afb0435148dc7d884
MichalWilczek <mich.wilcz@gmail.com>
2021-06-04 13:18:48 +0200

    Add missing zero.

--- Unit1.cpp
+++ Unit1.cpp
@@ -28,12 +28,15 @@ void __fastcall TForm1::Timer1Timer(TObject *Sender)
 
     hours = seconds / 3600;
     h = IntToStr(hours);
+    if (hours < 10) h = "0" + h;
 
     minutes = (seconds - hours * 3600) / 60;
     min = IntToStr(minutes);
+    if (minutes < 10) min = "0" + min;
 
     secondsLocal = seconds - hours * 3600 - minutes * 60;
     sec = IntToStr(secondsLocal);
+    if (secondsLocal < 10) sec = "0" + sec;
 
     Label1->Caption = h + ":" + min + ":" + sec;
 
